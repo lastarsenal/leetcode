2a631692249b381e0fa0714256d90253ec1ddcc8
yixun <ekixun@localhost>
2015-03-07 02:57:21 +0800

    MOD: add comment.

--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
 /**
- * Definition for singly-linked list.
+ * =====================================================================================
+ *       Filename:  ReverseLinkedListII.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015-03-07
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yixun 
+ *
+ * =====================================================================================
+
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+
+Hide Tags Linked List
+
  */
 struct ListNode {
   int val;
