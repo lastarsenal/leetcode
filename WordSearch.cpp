80238774647552b9e079bdf3c56717e10c8c1e95
yixun <yixun@dev2.corp.qihoo.net>
2015-03-06 14:31:31 +0800

    MOD: add file comment and problem description.

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -1,3 +1,38 @@
+/*
+ * =====================================================================================                                                                                                                                                    
+ *
+ *       Filename:  WordSearch.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015-03-06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yixun 
+ *
+ * =====================================================================================
+
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+For example,
+Given board =
+
+[
+  ["ABCE"],
+  ["SFCS"],
+  ["ADEE"]
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+Hide Tags Array Backtracking
+
+*/
+
 #include <stdio.h>
 #include <vector>
 #include <string>
